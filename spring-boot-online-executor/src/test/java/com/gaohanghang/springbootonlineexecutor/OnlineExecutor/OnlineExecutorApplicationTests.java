2bdb93626af9f17ff193d755c9031ede1964e750
gaohanghang <1341947277@qq.com>
2019-04-14 22:48:04 +0800

    <更新>(project-practice): 更新project-practice里的项目

---
+++ spring-boot-online-executor/src/test/java/com/gaohanghang/springbootonlineexecutor/OnlineExecutor/OnlineExecutorApplicationTests.java
@@ -0,0 +1,17 @@
+package com.gaohanghang.springbootonlineexecutor.OnlineExecutor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OnlineExecutorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
